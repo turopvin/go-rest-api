167db4582bf05557034c7fb4333007654e33e0d4
Artsiom Rudziak <turopvin@mail.ru>
2020-04-06 17:27:49 +0300

    connection to db

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -3,6 +3,7 @@ package apiserver
 import (
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
+	"github.com/turopvin/go-rest-api/internal/app/store"
 	"io"
 	"net/http"
 )
@@ -11,6 +12,7 @@ type APIServer struct {
 	config *Config
 	logger *logrus.Logger
 	router *mux.Router
+	store  *store.Store
 }
 
 func New(config *Config) *APIServer {
@@ -29,6 +31,10 @@ func (s *APIServer) Start() error {
 	s.logger.Info("Api server started successfully")
 	s.configureRouter()
 
+	if err := s.configureStore(); err != nil {
+		return err
+	}
+
 	return http.ListenAndServe(s.config.BindAddr, s.router)
 }
 
@@ -45,6 +51,17 @@ func (s *APIServer) configureLogger() error {
 
 func (s *APIServer) configureRouter() {
 	s.router.HandleFunc("/hello", s.handleHello())
+}
+
+func (s *APIServer) configureStore() error {
+	st := store.New(s.config.Store)
+	if err := st.Open(); err != nil {
+		return err
+	}
+
+	s.store = st
+
+	return nil
 }
 
 func (s *APIServer) handleHello() http.HandlerFunc {
---
+++ internal/app/store/store.go
@@ -0,0 +1,34 @@
+package store
+
+import "database/sql"
+import _ "github.com/go-sql-driver/mysql"
+
+type Store struct {
+	config *Config
+	db     *sql.DB
+}
+
+func New(config *Config) *Store {
+	return &Store{
+		config: config,
+	}
+}
+
+func (s *Store) Open() error {
+	db, err := sql.Open("mysql", s.config.DataBaseURL)
+	if err != nil {
+		return nil
+	}
+
+	if err := db.Ping(); err != nil {
+		return err
+	}
+
+	s.db = db
+
+	return nil
+}
+
+func (s *Store) Close() {
+	s.db.Close()
+}
