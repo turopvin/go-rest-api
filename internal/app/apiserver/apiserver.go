a5cbd9efe6db2cc45ecc70e59c999d0e78431aec
Artsiom Rudziak <turopvin@mail.ru>
2020-04-08 14:56:12 +0300

    add implementation for create and find user

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,8 +1,11 @@
 package apiserver
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
+	"github.com/turopvin/go-rest-api/internal/app/model"
 	"github.com/turopvin/go-rest-api/internal/app/store"
 	"io"
 	"net/http"
@@ -50,7 +53,10 @@ func (s *APIServer) configureLogger() error {
 }
 
 func (s *APIServer) configureRouter() {
+	s.router.HandleFunc("/", s.handleHome())
 	s.router.HandleFunc("/hello", s.handleHello())
+	s.router.HandleFunc("/user/create", s.handleUserCreate()).Methods("POST")
+	s.router.HandleFunc("/user/get", s.handleGetUser())
 }
 
 func (s *APIServer) configureStore() error {
@@ -64,8 +70,53 @@ func (s *APIServer) configureStore() error {
 	return nil
 }
 
+func (s *APIServer) handleHome() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		s.store.InitCollections()
+		w.WriteHeader(http.StatusOK)
+	}
+}
+
 func (s *APIServer) handleHello() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, "Hello")
+	}
+}
+
+func (s *APIServer) handleUserCreate() http.HandlerFunc {
+	type request struct {
+		FullName string `json:"full_name"`
+		Password string `json:"password"`
+		Email    string `json:"email"`
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		req := &request{}
+		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
+			return
+		}
+
+		u := &model.User{
+			FullName: req.FullName,
+			Password: req.Password,
+			Email:    req.Email,
+		}
+
+		u, _ = s.store.UserService().Create(u)
+		fmt.Println("nfd")
+	}
+}
+
+func (s *APIServer) handleGetUser() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		email := r.URL.Query().Get("email")
+		if email == "" {
+			s.logger.Debug("")
+		}
+		user, err := s.store.UserService().FindByEmail(email)
+		if err != nil {
+			io.WriteString(w, "No such user found")
+		}
+		json.NewEncoder(w).Encode(user)
 	}
 }
--- internal/app/store/userrepository.go
+++ internal/app/store/userrepository.go
@@ -1,15 +1,33 @@
 package store
 
-import "github.com/turopvin/go-rest-api/internal/app/model"
+import (
+	"context"
+	"github.com/turopvin/go-rest-api/internal/app/model"
+	"go.mongodb.org/mongo-driver/bson"
+	"log"
+)
 
 type UserRepository struct {
 	store *Store
 }
 
 func (r *UserRepository) Create(u *model.User) (*model.User, error) {
-	return nil, nil
+	collection := r.store.db.Database("dev").Collection("users")
+	_, err := collection.InsertOne(context.TODO(), u)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	return u, nil
 }
 
 func (r *UserRepository) FindByEmail(email string) (*model.User, error) {
-	return nil, nil
+	var user model.User
+	filter := bson.D{{"email", email}}
+	collection := r.store.db.Database("dev").Collection("users")
+	err := collection.FindOne(context.TODO(), filter).Decode(&user)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
 }
