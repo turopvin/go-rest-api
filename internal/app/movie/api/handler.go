245ac1dffb7f2f9bdebe6c67b12a6b7ba4e4f8fb
Artsiom Rudziak <turopvin@mail.ru>
2020-04-16 18:32:16 +0300

    fix movie api handler

--- internal/app/movie/api/handler.go
+++ internal/app/movie/api/handler.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"github.com/turopvin/go-rest-api/internal/app/apiserver"
 	"github.com/turopvin/go-rest-api/internal/app/movie"
 	"net/http"
 )
@@ -15,10 +16,11 @@ func New(useCase movie.UseCase) *Handler {
 
 func (h *Handler) findByTitle() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		title, err := h.useCase.FindByTitle("")
+		movies, err := h.useCase.FindByTitle("")
 		if err != nil {
-
+			apiserver.SendError(w, r, http.StatusNotFound, nil)
 		}
+		apiserver.SendRespond(w, r, http.StatusOK, movies)
 
 	}
 }
