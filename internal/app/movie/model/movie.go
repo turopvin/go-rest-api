0403b5df1b1b42498b3786bcfb7aa9c7a11a75af
Artsiom Rudziak <turopvin@mail.ru>
2020-04-22 15:07:38 +0300

    add omdb movie structure

--- internal/app/movie/model/movie.go
+++ internal/app/movie/model/movie.go
@@ -4,6 +4,7 @@ package model
 type ResponseMovie struct {
 	Title       string
 	ReleaseDate string
+	Director    string
 }
 
 type TmdbMovie struct {
@@ -21,6 +22,24 @@ type TmdbMovie struct {
 	VoteAverage      float32 `json:"vote_average"`
 	Overview         string  `json:"overview"`
 	ReleaseDate      string  `json:"release_date"`
+}
+
+type OmdbMovie struct {
+	Title      string `json:"Title"`
+	Year       string `json:"Year"`
+	Rated      string `json:"Rated"`
+	Released   string `json:"Released"`
+	Runtime    string `json:"Runtime"`
+	Genre      string `json:"Genre"`
+	Director   string `json:"Director"`
+	Writer     string `json:"Writer"`
+	Actors     string `json:"Actors"`
+	Plot       string `json:"Plot"`
+	Language   string `json:"Language"`
+	Country    string `json:"Country"`
+	Awards     string `json:"Awards"`
+	Poster     string `json:"Poster"`
+	ImdbRating string `json:"imdbRating"`
 }
 
 type ChannelMovie struct {
